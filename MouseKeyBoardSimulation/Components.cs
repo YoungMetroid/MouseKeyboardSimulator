fb21455351015ab9e599ffcdf21082d808ad418c
Felipe Elizalde <elizalde9@LIVE.COM>
2025-08-21 12:44:50 -0600

    Created a Components class which will just help me reuse and create different components and allow me to add them to the Main Form

---
+++ MouseKeyBoardSimulation/Components.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseKeyBoardSimulation
+{
+    class Components
+    {
+
+        public static TextBox CreateInput(string placeholder, Point location, Size size)
+        {
+            var box = new TextBox
+            {
+                Location = location,
+                Size = size,
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical,
+                Text = placeholder
+            };
+            return box;
+        }
+        public static Button CreateButton(Point location)
+        {
+            var button = new Button
+            {
+                Location = location
+               
+            };
+            return button; 
+        }
+
+    }
+}
